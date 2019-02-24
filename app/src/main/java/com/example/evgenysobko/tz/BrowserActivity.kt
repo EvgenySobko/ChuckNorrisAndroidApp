1a0aa54a3490299f049625950deda0bbbf2d17d5
evgenysobko <jooohn127@gmail.com>
2019-02-24 18:37:42 +0300

    ¯\_(ツ)_/¯

--- app/src/main/java/com/example/evgenysobko/tz/BrowserActivity.kt
+++ app/src/main/java/com/example/evgenysobko/tz/BrowserActivity.kt
@@ -2,7 +2,6 @@ package com.example.evgenysobko.tz
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
@@ -12,12 +11,26 @@ import android.view.MenuItem
 import android.webkit.WebView
 import kotlinx.android.synthetic.main.activity_browser.*
 import kotlinx.android.synthetic.main.app_bar_browser.*
+import android.webkit.WebResourceRequest
+import android.os.Build
+import android.annotation.TargetApi
+import android.webkit.WebViewClient
 
-class BrowserActivity : MainActivity() {
+
+class BrowserActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private var webView: WebView? = null
 
+    private inner class MyWebViewClient : WebViewClient() {
+        @TargetApi(Build.VERSION_CODES.N)
+        override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+            view.loadUrl(request.url.toString())
+            return true
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_browser)
         setSupportActionBar(toolbar)
@@ -25,8 +38,10 @@ class BrowserActivity : MainActivity() {
         webView = findViewById(R.id.webView)
         webView!!.settings.javaScriptEnabled
         webView!!.loadUrl("http://www.icndb.com/api/")
+        webView!!.webViewClient = MyWebViewClient()
 
-        val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
@@ -34,24 +49,26 @@ class BrowserActivity : MainActivity() {
     }
 
     override fun onBackPressed() {
-        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
-            drawer_layout.closeDrawer(GravityCompat.START)
+        if (webView!!.canGoBack()) {
+            webView!!.goBack()
         } else {
             super.onBackPressed()
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.main, menu)
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.browser, menu)
         return true
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_camera -> {
+            R.id.nav_jokes -> {
                 startActivity(Intent(this, MainActivity::class.java))
             }
-            R.id.nav_gallery -> {
+            R.id.nav_info -> {
                 startActivity(Intent(this, BrowserActivity::class.java))
             }
         }
--- app/src/main/java/com/example/evgenysobko/tz/MainActivity.kt
+++ app/src/main/java/com/example/evgenysobko/tz/MainActivity.kt
@@ -2,17 +2,29 @@ package com.example.evgenysobko.tz
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
+import android.support.design.widget.TextInputLayout
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import android.view.*
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
 
 open class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private val url = "http://api.icndb.com/jokes/random/"
+    private var jokes: MutableList<String> = mutableListOf()
+    private var count: Int = 0
+    private var okHttpClient: OkHttpClient = OkHttpClient()
+    private var inputtedText: TextInputLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +35,42 @@ open class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
-
         nav_view.setNavigationItemSelectedListener(this)
+
+        inputtedText = findViewById(R.id.input_text)
+
+        button2.setOnClickListener {
+            jokes.clear()
+            count = inputtedText!!.editText!!.text.toString().toInt()
+            if (count == 0) {
+                Toast.makeText(applicationContext, "Enter the correct number of jokes", Toast.LENGTH_LONG).show()
+            }
+            else {
+                for (i in 1..count) {
+                    loadRandomFact()
+                }
+            }
+        }
+        val listView: ListView = findViewById(R.id.list_of_jokes)
+        val adapter: ArrayAdapter<String>
+                = ArrayAdapter(this, android.R.layout.simple_list_item_1, jokes)
+        listView.adapter = adapter
+    }
+
+    private fun loadRandomFact() {
+        val request: Request = Request.Builder().url(url).build()
+        okHttpClient.newCall(request).enqueue(object : Callback {
+
+            override fun onFailure(call: Call?, e: IOException?) {}
+
+            override fun onResponse(call: Call?, response: Response?) {
+                val json = response?.body()?.string()
+                val txt = (JSONObject(json).getJSONObject("value").get("joke")).toString()
+                jokes.add(txt)
+                jokes.toString().replace("&quot", "")
+            }
+        })
+
     }
 
     override fun onBackPressed() {
@@ -36,22 +82,19 @@ open class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
 
-
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.nav_camera -> {
+            R.id.nav_jokes -> {
                 startActivity(Intent(this, MainActivity::class.java))
             }
-            R.id.nav_gallery -> {
+            R.id.nav_info -> {
                 startActivity(Intent(this, BrowserActivity::class.java))
-                }
             }
-
+        }
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
     }
